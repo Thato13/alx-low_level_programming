4fad52c1326fb704d0b82d96b4b507e5ae57028d
root <r16cmadalak1@gmail.com>
2022-09-11 06:26:20 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+int number_1 = 0;
+int number_2 = 1;
+
+while(number_1 <= 8)
+{
+
+while(number_2 <= 9)
+{
+putchar(number_1 + '0');
+putchar(number_2 + '0');
+
+if(number_2 < 9)
+{
+putchar(',');
+}
+putchar(' ');
+number_2++;
+}
+number_2 = 1;
+number_1++;
+}
+putchar('\n');
+return(0);
+}
