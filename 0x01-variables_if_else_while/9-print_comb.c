f3918241a988e7ff1763aee9f31a845b8f7d525b
root <r16cmadalak1@gmail.com>
2022-09-11 05:19:46 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+int number = 0;
+
+while(number <= 9)
+{
+
+putchar(number + '0');
+
+if(number <9)
+{
+putchar(',');
+}
+
+putchar(' ');
+number++;
+}
+
+putchar('\n');
+return(0);
+}
