799f63923bc5c392f18e3b83ba9c384ada96cbbd
root <r16cmadalak1@gmail.com>
2022-09-13 07:25:39 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include "main.h"
-
 
 int main(void)
 {
-c[] = "_putchar";
+char c[] = "_putchar";
 int n = strlen(c);
-int i = 0;
+int i =0;
 while(i <= n)
 {
 _putchar(c[i]);
+i++;
 }
 _putchar('\n');
 return(0);
