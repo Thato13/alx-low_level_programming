f992e6e935e79d04ede335d7b216430819f640e2
root <r16cmadalak1@gmail.com>
2022-09-14 01:40:35 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,103 @@
 #include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
 #ifndef main
 
+int _isalpha(char c)
+{
+if(c <= 122 && c >= 65)
+{
+printf("is alphabet");
+return 1;
+}
+return(0);
+}
+
+int print_sign(int n)
+{
+if(n > 0)
+{
+printf("+");
+return 1;
+}else if(n == 0)
+{
+printf("0");
+return 0;
+}else
+printf("-");
+return -1;
+}
+
+int _abs(int n)
+{
+if(n<0)
+{
+return -(n);
+}
+return n;
+}
 int _putchar(char c)
 {
 return(write(1, &c, 1));
 }
 
+int print_last_digit(int n)
+{
+
+if(n>=0)
+{
+_putchar((n % 10) + '0');
+}else if(n < 0)
+{
+_putchar((-(n) % 10) + '0');
+return (-(n) % 10);
+}
+return(n % 10); 
+}
+
+
+void print_alphabet(void)
+{
+char alphabet = 'a';
+while(alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n');
+}
+
+void print_alphabet_x10(void)
+{
+char alphabet = 'a';
+int n = 0;
+while(n <= 9)
+{
+while(alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n');
+n++;
+alphabet = 'a';
+}
+
+}
+
+int _islower(char c)
+{
+int checker = c;
+
+if(checker <= 122 && checker >= 97)
+{
+printf("is lower");
+return 1;
+
+}
+
+
+return(0);
+}
 #endif
 
