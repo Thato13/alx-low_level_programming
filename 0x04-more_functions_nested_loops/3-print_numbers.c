3ff57a5230b48963c8b46974be675afd00c02866
root <r16cmadalak1@gmail.com>
2022-09-15 06:42:23 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *main check code
+ *
+ *return 0 always
+ */
+int main(void)
+{
+print_numbers();
+return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -226,6 +226,17 @@ alphabet++;
 _putchar('\n');
 }
 
+void print_numbers(void)
+{
+char number = '0';
+while(number <= '9')
+{
+_putchar(number);
+number++;
+}
+_putchar('\n');
+}
+
 void print_alphabet_x10(void)
 {
 char alphabet = 'a';
